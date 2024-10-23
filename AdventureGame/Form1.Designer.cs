eced0d3a56bf04243c38140e7820363f86473480
azfaansa448 <azfaansa448@ed.amdsb.ca>
2024-10-23 11:13:03 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Imagebox = new System.Windows.Forms.PictureBox();
+            this.textboxLabel = new System.Windows.Forms.Label();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Imagebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // Imagebox
+            // 
+            this.Imagebox.Location = new System.Drawing.Point(12, 12);
+            this.Imagebox.Name = "Imagebox";
+            this.Imagebox.Size = new System.Drawing.Size(661, 346);
+            this.Imagebox.TabIndex = 1;
+            this.Imagebox.TabStop = false;
+            // 
+            // textboxLabel
+            // 
+            this.textboxLabel.ForeColor = System.Drawing.Color.White;
+            this.textboxLabel.Location = new System.Drawing.Point(12, 365);
+            this.textboxLabel.Name = "textboxLabel";
+            this.textboxLabel.Size = new System.Drawing.Size(450, 145);
+            this.textboxLabel.TabIndex = 2;
+            this.textboxLabel.Text = ".";
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.ForeColor = System.Drawing.Color.White;
+            this.questionLabel.Location = new System.Drawing.Point(468, 365);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(205, 145);
+            this.questionLabel.TabIndex = 3;
+            this.questionLabel.Text = ".";
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(680, 12);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(172, 173);
+            this.option1Button.TabIndex = 4;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(679, 191);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(172, 173);
+            this.option2Button.TabIndex = 5;
+            this.option2Button.UseVisualStyleBackColor = true;
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(680, 370);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(172, 173);
+            this.option3Button.TabIndex = 6;
+            this.option3Button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(864, 553);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.textboxLabel);
+            this.Controls.Add(this.Imagebox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Imagebox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Imagebox;
+        private System.Windows.Forms.Label textboxLabel;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textboxLabel.Text = "A new case just came in, should I take it?";
+            questionLabel.Text = "Should I take the case?";
+            option1Button.Text = "Yes";
+            option2Button.Text = "No";
+            option3Button.Visible = false;
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if(page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                Random randGen = new Random();
+                int randomValue = randGen.Next(1, 101);
+                if(randomValue >= 80)
+                {
+                    page = 7;
+                }
+                else
+                {
+                    page = 5;
+                }
+            }
+            else if (page == 5)
+            {
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                page = 9;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 10)
+            {
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                page = 9;
+            }
+            else if (page == 12)
+            {
+                page = 9;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }else if (page == 2)
+            {
+                page = 4;
+            }
+            displayPage();
+        }
+
+        public void displayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    textboxLabel.Text = "A new case just came in, should I take it?";
+                    questionLabel.Text = "Should I take the case?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+                case 2:
+                    textboxLabel.Text = "Just looking at the file, it looks really complicated.";
+                    questionLabel.Text = "Should I read the case?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+                case 3:
+                    textboxLabel.Text = "But I need the money...";
+                    option1Button.Enabled = false;
+                    option2Button.Enabled = false;
+                    Refresh();
+                    Thread.Sleep(1000);
+                    option1Button.Enabled = true;
+                    option2Button.Enabled = true;
+                    questionLabel.Text = "Should I take the case?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+            }
+        }
+    }
+}
